ac14fec52b43f87fefb841f043bed38e9fd5cdcd
Scott McCoy <scotthmccoy@gmail.com>
2020-10-12 16:57:42 -0700

    chore: Created project
    Also, added default gitignore from https://github.com/github/gitignore/blob/master/Swift.gitignore

---
+++ HappyThingsLog/HappyThingsLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  HappyThingsLogApp.swift
+//  HappyThingsLog
+//
+//  Created by Scott McCoy on 10/12/20.
+//
+
+import SwiftUI
+
+@main
+struct HappyThingsLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
